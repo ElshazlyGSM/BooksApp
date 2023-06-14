9fbb6dd2bce6e8e053fdd303b6cde826a1598caf
Administrator <elshazly,gsm@gmail.com>
2023-06-14 17:32:59 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/features/Splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      home: SplashView(),
+    );
+  }
+}
